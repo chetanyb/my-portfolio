66633b97fb8b85abd0b4eb61762963443f5e6807
chetanyb <techbuddy022@gmail.com>
2024-02-03 23:31:18 +0530

    Update project showcase portfolio page with description, conditional rendering details and a new layout

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,10 +1,43 @@
 const projects = [
 	{
 		slug: 'project1',
-		title: 'Project One',
+		title: 'A long project name',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.',
-		links: { github: '' }
+		links: { github: '', ethGlobal: '', devpost: '', youtube: '', news: '', website: '' },
+		details: {
+			role: 'Leader',
+			team: 'Member1, Member2, Member3',
+			technologies: '',
+			accomplishments: ''
+		},
+		//copilot generate long sample text
+		elaboration:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.\
+		Aliquam erat volutpat In id fermentum augue, ut pellentesque leo. Maecenas at arcu risus. Donec commodo sodales ex, scelerisque laoreet nibh hendrerit id.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.\
+		Aliquam erat volutpat In id fermentum augue, ut pellentesque leo. Maecenas at arcu risus. Donec commodo sodales ex, scelerisque laoreet nibh hendrerit id.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.\
+		Aliquam erat volutpat In id fermentum augue, ut pellentesque leo. Maecenas at arcu risus. Donec commodo sodales ex, scelerisque laoreet nibh hendrerit id.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.\
+		Aliquam erat volutpat In id fermentum augue, ut pellentesque leo. Maecenas at arcu risus. Donec commodo sodales ex, scelerisque laoreet nibh hendrerit id.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.\
+		Aliquam erat volutpat In id fermentum augue, ut pellentesque leo. Maecenas at arcu risus. Donec commodo sodales ex, scelerisque laoreet nibh hendrerit id.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.\
+		Proin ultrices tortor non quam feugiat, eu luctus est lacinia. Proin ultrices tortor non quam feugiat, eu luctus est lacinia.'
 	},
 	{
 		slug: 'project2',
--- src/routes/portfolio/[slug]/+page.svelte
+++ src/routes/portfolio/[slug]/+page.svelte
@@ -6,49 +6,65 @@
 
 <article>
 	<div class="flex items-center">
-		<h1 class="h1">{data.project?.slug}</h1>
-
-		<div class="logo-cloud grid-cols-1 lg:!grid-cols-3 gap-1 px-12 flex flex-auto">
+		<h1 class="h1">{data.project?.title}</h1>
+		<div class="logo-cloud grid-cols-1 lg:!grid-cols-3 gap-1 pl-8 flex flex-auto">
 			<a class="logo-item variant-ghost" href={data.project?.links?.github}>
 				<img src="/github.svg" alt="github-logo" class="h-5" />
 				<span>GitHub</span>
 			</a>
 			<a class="logo-item variant-ghost flex justify-center" href="/">
-				<img src="/ethglobal.svg" class="h-5" />
+				<img src="/ethglobal.svg" alt="ethglobal-logo" class="h-5" />
 				<span>ETHGlobal</span>
 			</a>
 			<a class="logo-item variant-ghost flex items-center justify-center" href="/">
-				<img src="/ethglobal.svg" class="h-5" />
+				<img src="/ethglobal.svg" alt="ethglobal-logo" class="h-5" />
 			</a>
 			<a class="logo-item variant-ghost flex items-center justify-center" href="/">
-				<img src="/ethglobal.svg" class="h-5" />
+				<img src="/ethglobal.svg" alt="ethglobal-logo" class="h-5" />
 			</a>
 			<a class="logo-item variant-ghost flex justify-center" href="/">
-				<img src="/ethglobal.svg" class="h-5" />
+				<img src="/ethglobal.svg" alt="ethglobal-logo" class="h-5" />
 				<span>ETHGlobal</span>
 			</a>
 			<a class="logo-item variant-ghost flex justify-center" href="/">
-				<img src="/ethglobal.svg" class="h-5" />
+				<img src="/ethglobal.svg" alt="ethglobal-logo" class="h-5" />
 				<span>ETHGlobal</span>
 			</a>
 			<a class="logo-item variant-ghost flex justify-center" href="/">
-				<img src="/ethglobal.svg" class="h-5" />
+				<img src="/ethglobal.svg" alt="ethglobal-logo" class="h-5" />
 				<span>ETHGlobal</span>
 			</a>
 			<a class="logo-item variant-ghost flex justify-center" href="/">
-				<img src="/ethglobal.svg" class="h-5" />
+				<img src="/ethglobal.svg" alt="ethglobal-logo" class="h-5" />
 				<span>ETHGlobal</span>
 			</a>
 		</div>
 	</div>
+	<h4 class="h4 py-6">{data.project?.description}</h4>
 
-	<div
-		class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10"
-	>
+	<div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 pb-4">
 		{#each Array.from({ length: 8 }) as _, i}
 			<div class="snap-start shrink-0 card py-20 w-40 md:w-80 text-center">{i + 1}</div>
 		{/each}
 	</div>
 
-	<p class="py-8">{data.project?.description}</p>
+	<div class="flex-row">
+		<div class="pt-8">
+			<p>{data.project?.elaboration}</p>
+		</div>
+		<hr class="!border-t-2 my-6 !border-dashed" />
+		<div class="flex items-center">
+			{#if data.project?.details}
+				{#each Object.entries(data.project?.details) as [key, value]}
+					<div class="px-2">
+						<p class="h6">{key}</p>
+						<p>{value}</p>
+					</div>
+					{#if key !== Object.keys(data.project?.details)[Object.keys(data.project?.details).length - 1]}
+						<span class="divider-vertical h-20 !border-dashed" />
+					{/if}
+				{/each}
+			{/if}
+		</div>
+	</div>
 </article>
