4355003b5c910062de11897061a0ec9223c118c2
chetanyb <techbuddy022@gmail.com>
2024-02-08 23:54:37 +0530

    added namaste cursor on hovering text

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -1,14 +1,14 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
 <div>
-	<div class="flex justify-center group">
+	<div class="flex justify-center group namaaste-cursor">
 		<h1 class="h1 my-2 block group-hover:hidden">Hello! Nice to meet you!</h1>
 		<h1 class="h1 my-4 font-kalam font-bold hidden group-hover:block">
 			नमस्ते! आपसे मिलकर अच्छा लगा!
 		</h1>
 	</div>
 </div>
+
+<style>
+	.namaaste-cursor:hover {
+		cursor: url('cursor-namaste.png'), auto;
+	}
+</style>
