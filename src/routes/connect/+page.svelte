444946f63abad3f49f0334d029546d64d4f6744c
chetanyb <techbuddy022@gmail.com>
2024-02-08 23:25:56 +0530

    Added connect page font change on hover

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -5,8 +5,10 @@
 </script>
 
 <div>
-	<div class="flex justify-center">
-		<h1 class="h1 my-2">Hello! Nice to meet you!</h1>
-		<h1 class="h1 my-4 font-kalam font-bold">नमस्ते! आपसे मिलकर अच्छा लगा!</h1>
+	<div class="flex justify-center group">
+		<h1 class="h1 my-2 block group-hover:hidden">Hello! Nice to meet you!</h1>
+		<h1 class="h1 my-4 font-kalam font-bold hidden group-hover:block">
+			नमस्ते! आपसे मिलकर अच्छा लगा!
+		</h1>
 	</div>
 </div>
