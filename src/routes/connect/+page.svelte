692996e49211ca8e78a3c7bcc537a09ad08bd30d
chetanyb <techbuddy022@gmail.com>
2024-02-09 11:54:43 +0530

    Add unselectable class to prevent text selection

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -1,5 +1,5 @@
 <div>
-	<div class="flex justify-center group namaaste-cursor">
+	<div class="flex justify-center group namaaste-cursor unselectable">
 		<h1 class="h1 my-0 font-love_notes text-6xl block group-hover:hidden">
 			Hello! Nice to meet you!
 		</h1>
@@ -12,5 +12,11 @@
 <style>
 	.namaaste-cursor:hover {
 		cursor: url('cursor-namaste.png'), auto;
+	}
+	.unselectable {
+		-webkit-user-select: none; /* Safari */
+		-moz-user-select: none; /* Firefox */
+		-ms-user-select: none; /* Internet Explorer/Edge */
+		user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera, and Firefox */
 	}
 </style>
