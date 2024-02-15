97b5c64af2b28eb18605520d69ada1f8f9572fee
chetanyb <techbuddy022@gmail.com>
2024-02-16 00:28:25 +0530

    Created scrollable lens feed

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -2,19 +2,23 @@
 	import { onMount } from 'svelte';
 
 	let twitterHeight: number;
+	let twitterWidth: number;
 	export let data;
 
-	function setTwitterHeight() {
+	function setTwitterDimension() {
 		const vh = window.innerHeight;
+		const vw = window.innerWidth;
 		twitterHeight = (window.innerWidth <= 768 ? 0.4 : 0.75) * vh;
+		console.log(twitterHeight);
+		console.log(twitterWidth);
 	}
 
 	onMount(() => {
-		setTwitterHeight();
-		window.addEventListener('resize', setTwitterHeight);
+		setTwitterDimension();
+		window.addEventListener('resize', setTwitterDimension);
 
 		return () => {
-			window.removeEventListener('resize', setTwitterHeight);
+			window.removeEventListener('resize', setTwitterDimension);
 		};
 	});
 </script>
@@ -50,14 +54,35 @@
 				data-chrome="transparent noscrollbar noborders"
 				href="https://twitter.com/BRUHDWAJ?ref_src=twsrc%5Etfw"
 			></a>
-			<!--<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>-->
+			<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 		</div>
-		<div
-			class="flex-col mt-12 mx-2 h-[40vh] lg:w-[50vw] card overflow-scroll hide-scrollbar rounded-md !bg-transparent"
-		>
-			<!-- svelte-ignore a11y-missing-content -->
-			<div class="flex flex-row rounded-md">
+
+		<!-- svelte-ignore a11y-missing-content -->
+		<div class="mt-12 mx-2 h-[40vh] lg:w-[50vw] card rounded-md !bg-transparent flex flex-col">
+			<div class="flex flex-row h-24">
 				<h4 class="h4 text-white !bg-transparent p-2 border-b-0">World Through My Lens</h4>
+			</div>
+			<div class="flex-grow overflow-auto mx-2 hide-scrollbar">
+				{data.profile.id}<br />
+				{data.profile.metadata.bio}<br />
+				{data.profile.metadata.displayName}<br />
+				{data.profile.metadata.picture.optimized.uri}<br />
+				{data.profile.stats.followers}<br />
+				{data.profile.stats.following}<br />
+				{data.profile.id}<br />
+				{data.profile.metadata.bio}<br />
+				{data.profile.metadata.displayName}<br />
+				{data.profile.metadata.picture.optimized.uri}<br />
+				{data.profile.stats.followers}<br />
+				{data.profile.stats.following}<br />
+
+				{#each data.publications.items as post}
+					{#if post.metadata}
+						{#if post.metadata.content}
+							<div>{post.metadata.content}</div>
+						{/if}
+					{/if}
+				{/each}
 			</div>
 		</div>
 	</div>
