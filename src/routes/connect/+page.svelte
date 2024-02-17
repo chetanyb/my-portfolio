6a8fd372a5a3a78f30d5168244a06ef4cfab8f9c
chetanyb <techbuddy022@gmail.com>
2024-02-17 15:18:16 +0530

    Update LinkedIn logo background opacity

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -142,7 +142,7 @@
 			class="mx-2 h-[40vh] lg:w-[50vw] card rounded-md !bg-transparent flex flex-col variant-outline"
 		>
 			<div class="flex flex-col border rounded-lg border-slate-500 border-opacity-25 m-2">
-				<div class="flex items-center bg-white bg-opacity-5 rounded-t-lg">
+				<div class="flex items-center bg-white bg-opacity-10 rounded-t-lg">
 					<img src="linkedin.png" alt="linkedin" class="h-12 p-2" />
 				</div>
 				<div class="flex flex-col bg-black rounded-b-lg bg-opacity-15">
