a9e16a260ae010a2b206a80f6f0ebda99366831d
chetanyb <techbuddy022@gmail.com>
2024-02-17 15:12:23 +0530

    Update connect page layout and added LinkedIn profile card

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 
 	let twitterHeight: number;
+
 	export let data;
 	const publications = data.publications.items;
 	const filteredPublications = publications
@@ -37,7 +39,7 @@
 		class="flex flex-row justify-center group namaaste-cursor unselectable pb-2 md:pb-6 lg:pb-10 h-auto relative"
 	>
 		<h1
-			class="text-4xl md:text-6xl flex font-love_notes justify-center py-3 absolute top-0 left-0 right-0 mx-auto text-center lg:py-6 group-hover:hidden"
+			class="text-4xl md:text-6xl flex font-love_notes justify-center absolute top-0 left-0 right-0 mx-auto text-center lg:py-4 group-hover:hidden"
 		>
 			Hello!<span class="hidden min-[350px]:block"> Nice to meet you!</span>
 		</h1>
@@ -48,10 +50,10 @@
 		</h1>
 	</div>
 	<div
-		class="flex-col md:flex-row md:flex w-full justify-center hide-scrollbar min-[350px]:my-2 md:my-6 lg:my-12"
+		class="flex-col md:flex-row md:flex w-full justify-center hide-scrollbar min-[350px]:mt-2 md:ty-6 lg:mt-12"
 	>
 		<div
-			class="mt-12 mx-2 h-[40vh] lg:w-[50vw] card overflow-scroll hide-scrollbar rounded-md !bg-transparent"
+			class="mx-2 h-[40vh] lg:w-[50vw] card overflow-scroll hide-scrollbar rounded-md !bg-transparent variant-outline"
 		>
 			<!-- svelte-ignore a11y-missing-content -->
 			<a
@@ -65,7 +67,9 @@
 			<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 		</div>
 
-		<div class="mt-12 mx-2 h-[40vh] lg:w-[50vw] card rounded-md !bg-transparent flex flex-col">
+		<div
+			class="mx-2 h-[40vh] lg:w-[50vw] card rounded-md !bg-transparent flex flex-col variant-outline"
+		>
 			<button
 				class="w-full hover:bg-green-700 hover:bg-opacity-10 transition-colors duration-500"
 				on:click={handleLensProfileClick}
@@ -133,16 +137,82 @@
 			</div>
 		</div>
 	</div>
+	<div class="flex py-4">
+		<div
+			class="mx-2 h-[40vh] lg:w-[50vw] card rounded-md !bg-transparent flex flex-col variant-outline"
+		>
+			<div class="flex flex-col border rounded-lg border-slate-500 border-opacity-25 m-2">
+				<div class="flex items-center bg-white bg-opacity-5 rounded-t-lg">
+					<img src="linkedin.png" alt="linkedin" class="h-12 p-2" />
+				</div>
+				<div class="flex flex-col bg-black rounded-b-lg bg-opacity-15">
+					<div class="flex flex-row items-center">
+						<img
+							src="https://media.licdn.com/dms/image/C4D03AQE1SIqATg94Lw/profile-displayphoto-shrink_200_200/0/1658836970805?e=1713398400&v=beta&t=rr9YJTZtdpenK1hmoQsZdIAqS_FnziK39xzRcx19iro"
+							alt="linkedin profile"
+							class="h-16 w-16 rounded-full mx-4 my-2"
+						/>
+						<div class="flex flex-col w-full">
+							<div class="flex flex-row justify-between">
+								<button
+									class="text-xl px-2 pt-4 hover:underline"
+									on:click={() =>
+										window.open('https://www.linkedin.com/in/chetanybhardwaj/, _blank')}
+									>Chetany Bhardwaj</button
+								>
+								<button
+									class="flex text-sm px-2 pt-5"
+									on:click={() =>
+										window.open('https://www.linkedin.com/school/upesdehradun/', '_blank')}
+								>
+									<img src="UPES.png" alt="upes" class="h-5" />
+									<p class="px-1 hover:underline">UPES</p>
+								</button>
+							</div>
+							<div class="group unselectable text-md px-2 py-1">
+								<p class="group-hover:hidden">
+									Software engineer | Blockchain Beever | Cloud Native | IoT Connected
+								</p>
+								<p class="hidden group-hover:block animate-bounce">LIFE ENTHUSIAST</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="my-1 mx-2">
+						<button
+							class="flex justify-center border rounded-full w-full inner-glow px-4"
+							on:click={() => window.open('https://www.linkedin.com/in/chetanybhardwaj/, _blank')}
+						>
+							<span class="hidden sm:block" style="margin-right: 4px;">View profile and</span
+							>Connect on Linkedin
+						</button>
+					</div>
+				</div>
+			</div>
+			<div>hello</div>
+		</div>
+		<div
+			class="mx-2 h-[40vh] lg:w-[50vw] card rounded-md !bg-transparent flex flex-col variant-outline"
+		>
+			<p>SWAG</p>
+		</div>
+	</div>
 </div>
 
 <style>
 	.namaaste-cursor:hover {
 		cursor: url('cursor-namaste.png'), auto;
 	}
+
 	.unselectable {
 		-webkit-user-select: none; /* Safari */
 		-moz-user-select: none; /* Firefox */
 		-ms-user-select: none; /* IE/Edge */
 		user-select: none; /* Chrome/Opera/Firefox */
+	}
+
+	.inner-glow:hover {
+		box-shadow: inset 0 0 5px #ffffff;
+		transition: box-shadow 0.3s ease-in-out;
 	}
 </style>
