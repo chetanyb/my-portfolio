ad0f9c563b59e680fb60c967055d6cfc6524cc8b
chetanyb <techbuddy022@gmail.com>
2024-02-24 14:37:27 +0530

    Changed flex direction to column for smaller screens

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -7,8 +7,8 @@
 </script>
 
 <div>
-	<div class="flex item-center my-10">
-		<div class="w-full flex absolute justify-center">
+	<div class="flex-col sm:flex justify-center item-center my-10">
+		<div class="w-full flex justify-center">
 			<h2 class="h2">
 				<span
 					class="bg-gradient-to-br from-yellow-500 to-yellow-700 bg-clip-text text-transparent box-decoration-clone"
@@ -17,13 +17,13 @@
 				</span>
 			</h2>
 		</div>
-		<div class="flex w-full justify-end mx-10">
+		<div class="flex items-center justify-center pt-2 lg:pt-0 lg:absolute lg:right-5 mx-10">
 			<span class="chip variant-ghost-surface mx-4 text-sm">Filter</span>
 		</div>
 	</div>
 	<!--Creating cards for each projects-->
-	<div class="w-screen flex px-10">
-		<div class="w-1/4 block card-hover card p-4 m-4 !bg-transparent">
+	<div class="w-screen flex-col sm:flex-row sm:flex px-10">
+		<div class="w-full sm:w-1/4 block card-hover card p-4 m-4 !bg-transparent">
 			<div
 				class="-m-4 h-16 rounded-sm bg-gradient-to-br from-yellow-500 to-yellow-700 rounded-t-sm rounded-b-none"
 			>
@@ -41,7 +41,7 @@
 			</div>
 		</div>
 		<button
-			class="w-1/4 block card-hover card p-4 m-4 !bg-transparent"
+			class="w-full sm:w-1/4 block card-hover card p-4 m-4 !bg-transparent"
 			on:click={() => navigateToProject('sita')}
 		>
 			<div
@@ -61,7 +61,7 @@
 			</div>
 		</button>
 
-		<div class="w-1/4 block card-hover card p-4 m-4 !bg-transparent">
+		<div class="w-full sm:w-1/4 block card-hover card p-4 m-4 !bg-transparent">
 			<div
 				class="-m-4 h-16 rounded-sm bg-gradient-to-br from-yellow-500 to-yellow-700 rounded-t-sm rounded-b-none"
 			>
@@ -78,7 +78,7 @@
 				<span class="badge variant-ghost mx-1">Blockchain</span>
 			</div>
 		</div>
-		<div class="w-1/4 block card-hover card p-4 m-4 !bg-transparent">
+		<div class="w-full sm:w-1/4 block card-hover card p-4 m-4 !bg-transparent">
 			<div
 				class="-m-4 h-16 rounded-sm bg-gradient-to-br from-yellow-500 to-yellow-700 rounded-t-sm rounded-b-none"
 			>
