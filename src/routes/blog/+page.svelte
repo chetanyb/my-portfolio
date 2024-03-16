6d675a2bae51679c7d6bc2291c5054540c4bd30a
chetanyb <techbuddy022@gmail.com>
2024-03-16 22:43:47 +0530

    Updated blog page layout

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,83 @@
-<h1>Blogs</h1>
+<script lang="ts">
+	//export let data;
+</script>
+
+<div>
+	<div class="flex w-screen justify-center py-2">
+		<h2 class="h2 p-4">Blogs</h2>
+	</div>
+	<div class="flex w-full justify-center px-5 py-2">
+		<div class="card card-hover m-2">
+			<img
+				src="https://via.placeholder.com/300x100"
+				alt="placeholder"
+				class="w-full filter invert"
+			/>
+			<p class="p-2 pl-4">Technology</p>
+			<p class="p-2 pl-4">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas in maxime suscipit
+				praesentium asperiores. Maxime natus reiciendis aperiam ea ipsum, ab fuga nam autem
+				explicabo itaque aut perferendis cupiditate commodi.
+			</p>
+			<hr />
+			<div class="flex flex-row justify-between py-2 px-4">
+				<p>By Chetany Bhardwaj</p>
+				<time datetime="2024-03-16">16 March, 2024</time>
+			</div>
+		</div>
+		<div class="card card-hover m-2">
+			<img
+				src="https://via.placeholder.com/300x100"
+				alt="placeholder"
+				class="w-full filter invert"
+			/>
+			<p class="p-2 pl-4">Technology</p>
+			<p class="p-2 pl-4">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas in maxime suscipit
+				praesentium asperiores. Maxime natus reiciendis aperiam ea ipsum, ab fuga nam autem
+				explicabo itaque aut perferendis cupiditate commodi.
+			</p>
+			<hr />
+			<div class="flex flex-row justify-between py-2 px-4">
+				<p>By Chetany Bhardwaj</p>
+				<time datetime="2024-03-16">16 March, 2024</time>
+			</div>
+		</div>
+		<div class="card card-hover m-2">
+			<img
+				src="https://via.placeholder.com/300x100"
+				alt="placeholder"
+				class="w-full filter invert"
+			/>
+			<p class="p-2 pl-4">Technology</p>
+			<p class="p-2 pl-4">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas in maxime suscipit
+				praesentium asperiores. Maxime natus reiciendis aperiam ea ipsum, ab fuga nam autem
+				explicabo itaque aut perferendis cupiditate commodi.
+			</p>
+			<hr />
+			<div class="flex flex-row justify-between py-2 px-4">
+				<p>By Chetany Bhardwaj</p>
+				<time datetime="2024-03-16">16 March, 2024</time>
+			</div>
+		</div>
+		<div class="card card-hover m-2">
+			<img
+				src="https://via.placeholder.com/300x100"
+				alt="placeholder"
+				class="w-full filter invert"
+			/>
+			<p class="p-2 pl-4">Technology</p>
+			<p class="p-2 pl-4">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas in maxime suscipit
+				praesentium asperiores. Maxime natus reiciendis aperiam ea ipsum, ab fuga nam autem
+				explicabo itaque aut perferendis cupiditate commodi.
+			</p>
+			<hr />
+			<div class="flex flex-row justify-between py-2 px-4">
+				<p>By Chetany Bhardwaj</p>
+				<time datetime="2024-03-16">16 March, 2024</time>
+			</div>
+		</div>
+	</div>
+</div>
