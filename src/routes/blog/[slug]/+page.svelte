4d20be0149051585a23cca0702b09f6a8cf3e7c2
chetanyb <techbuddy022@gmail.com>
2024-03-26 23:24:21 +0530

    Update blog page layout

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -4,5 +4,11 @@
 </script>
 
 <div>
-	<p>This is blog page</p>
+	<div class="w-full flex justify-between">
+		<div class="w-1/12 h-screen bg-gradient-to-br from-yellow-500 to-yellow-700" />
+		<div class="my-2">
+			<h2 class="h2">{data.blog?.title}</h2>
+		</div>
+		<div class="w-1/12 h-screen bg-gradient-to-br from-yellow-500 to-yellow-700" />
+	</div>
 </div>
