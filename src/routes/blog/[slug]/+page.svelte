a2d7bd92f5fbd6718b83257a4da2e722b410b1b0
chetanyb <techbuddy022@gmail.com>
2024-03-29 21:43:35 +0530

    Updated blog page layout

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -6,13 +6,17 @@
 <div>
 	<div class="w-full flex justify-between">
 		<div class="w-6 h-screen bg-gradient-to-br from-yellow-500 to-yellow-700" />
-		<div class="w-full flex-col items-center text-center my-2">
+		<div class="w-full text-center my-2">
 			<h2 class="h2 pt-4">{data.blog?.title}</h2>
 			<div class="flex justify-between my-2 mx-4">
 				<p>Chetany Bhardwaj</p>
 				<p>28 March, 2024</p>
 			</div>
-			<hr />
+			<hr class="!border-dashed" />
+			<div class="w-full flex flex-col items-center justify-center">
+				<img class="p-4" src="https://via.placeholder.com/800x400" alt="blog" />
+				<p class="px-2 text-justify">{data.blog?.description}</p>
+			</div>
 		</div>
 		<div class="w-6 h-screen bg-gradient-to-br from-yellow-500 to-yellow-700" />
 	</div>
