f6f0fb7be2ffd510eebf4a437fb7661ec8fae85e
chetanyb <techbuddy022@gmail.com>
2024-04-02 23:11:00 +0530

    Updated blog page for responsiveness

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -4,7 +4,7 @@
 
 <div>
 	<div class="w-full flex justify-between">
-		<div class="w-6 bg-gradient-to-br from-yellow-500 to-yellow-700" />
+		<div class="w-6 hidden md:block bg-gradient-to-br from-yellow-500 to-yellow-700" />
 		<div class="w-full text-center my-2">
 			<h2 class="h2 pt-4">{data.blog?.title}</h2>
 			<div class="flex justify-between my-2 mx-4">
@@ -17,6 +17,6 @@
 				<p class="px-2 text-justify">{@html data.blog?.description}</p>
 			</div>
 		</div>
-		<div class="w-6 bg-gradient-to-br from-yellow-500 to-yellow-700" />
+		<div class="w-6 hidden md:block bg-gradient-to-br from-yellow-500 to-yellow-700" />
 	</div>
 </div>
