08bdde3495df1ac06c356a316971235f453a367c
chetanyb <techbuddy022@gmail.com>
2024-04-15 22:58:05 +0530

    Integrate CMS API to fetch blogs in blog page

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,6 +1,13 @@
 //TODO: switch to CMS for this page
 import { STRAPI_URL, STRAPI_TOKEN } from '$env/static/private';
 
+//making a request to the strapi api
+const blogs = await fetch(`${STRAPI_URL}/blogs`, {
+	headers: {
+		Authorization: `Bearer ${STRAPI_TOKEN}`
+	}
+}).then((res) => res.json());
+
 export async function load({ params }) {
 	const blogName = params.slug;
 	const blog = blogs.find((blog) => blog.slug === blogName);
