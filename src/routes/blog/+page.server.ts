9655337992a52a543dd453bccb8ef3f6acc0eac9
chetanyb <techbuddy022@gmail.com>
2024-04-20 21:52:35 +0530

    Integrated CMS API to fetch blogs using Strapi

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,5 +1,13 @@
 import type { PageServerLoad } from './$types';
-import blogs from '$lib/blogs';
+//import blogs from '$lib/blogs';
+import { STRAPI_URL, STRAPI_TOKEN } from '$env/static/private';
+
+//making a request to the strapi api
+const blogs = await fetch(`${STRAPI_URL}/blogs`, {
+	headers: {
+		Authorization: `Bearer ${STRAPI_TOKEN}`
+	}
+}).then((res) => res.json());
 
 export const load = (async () => {
 	return { blogs };
