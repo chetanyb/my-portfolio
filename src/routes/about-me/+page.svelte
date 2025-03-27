87d2892685e7f195d68f06f9929e2709a93fca8b
chethack <chetanyiot@gmail.com>
2025-03-27 20:30:54 +0530

    updated zkSampler

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -11,36 +11,53 @@
 	<div class="m-2 px-4 py-2 pt-0">
 		<div class="flex-col">
 			<p class="text-justify">
-				Hi, I’m <strong>Chetany Bhardwaj</strong>, aka <strong>chethack</strong>—a passionate software engineer and creative thinker dedicated to building solutions that drive innovation and inclusivity. My journey in computer science is fueled by curiosity, a love for solving complex problems, and a desire to make a meaningful impact through technology. ✨
+				Hi, I’m <strong>Chetany Bhardwaj</strong>, aka <strong>chethack</strong>—a passionate
+				software engineer and creative thinker dedicated to building solutions that drive innovation
+				and inclusivity. My journey in computer science is fueled by curiosity, a love for solving
+				complex problems, and a desire to make a meaningful impact through technology. ✨
 			</p>
-			<br>
+			<br />
 			<p class="text-justify">
-				With hands-on experience in blockchain, cloud computing, and GRID technologies, I’ve honed my skills through impactful internships at <strong>Polygon Labs</strong> and <strong>KEK</strong>. From optimizing high-performance systems to leading award-winning hackathon projects like <strong>SITA</strong>, a blockchain-based agricultural lending platform, I’m committed to delivering scalable and ethical solutions. 🚀
+				With hands-on experience in blockchain, cloud computing, and GRID technologies, I’ve honed
+				my skills through impactful internships at <strong>Polygon Labs</strong> and
+				<strong>KEK</strong>. From optimizing high-performance systems to leading award-winning
+				hackathon projects like <strong>SITA</strong>, a blockchain-based agricultural lending
+				platform, I’m committed to delivering scalable and ethical solutions. 🚀
 			</p>
-			<br>
+			<br />
 			<p class="text-justify">
-				Currently, I’m a final-year <strong>Computer Science Engineering</strong> student majoring in <strong>Cloud Computation and Virtualization Technology</strong>, with dual minors in <strong>DevOps Practices</strong> and <strong>Design Principles</strong>. My academic journey, combined with certifications like <strong>AWS Solutions Architect</strong>, equips me with the expertise to tackle challenges in cloud computing, DevOps, and distributed systems. 🖥️
+				Currently, I’m a final-year <strong>Computer Science Engineering</strong> student majoring
+				in <strong>Cloud Computation and Virtualization Technology</strong>, with dual minors in
+				<strong>DevOps Practices</strong>
+				and <strong>Design Principles</strong>. My academic journey, combined with certifications
+				like <strong>AWS Solutions Architect</strong>, equips me with the expertise to tackle
+				challenges in cloud computing, DevOps, and distributed systems. 🖥️
 			</p>
-			<br>
+			<br />
 			<p class="text-justify">
-				Beyond technology, I channel my creativity into <strong>music production</strong>, <strong>songwriting</strong>, and <strong>rapping</strong>—expressing ideas and emotions through rhythm and lyrics. 🎵🎤
+				Beyond technology, I channel my creativity into <strong>music production</strong>,
+				<strong>songwriting</strong>, and <strong>rapping</strong>—expressing ideas and emotions
+				through rhythm and lyrics. 🎵🎤
 			</p>
-			<br>
+			<br />
 			<p class="text-justify">
 				Let’s connect and collaborate to build a future where innovation meets social good. 🌍🤝
 			</p>
 		</div>
-
 	</div>
 
-	<hr class="!border-t-2 !border-yellow-400 my-1">
+	<hr class="!border-t-2 !border-yellow-400 my-1" />
 
 	<div class="flex-col">
-		<h3 class="h3 bg-gradient-to-r from-yellow-400 to-yellow-700 px-4 py-1 text-orange-800">Experience</h3>
+		<h3 class="h3 bg-gradient-to-r from-yellow-400 to-yellow-700 px-4 py-1 text-orange-800">
+			Experience
+		</h3>
 		<div class="m-2">
 			<Accordion>
 				<AccordionItem>
-					<svelte:fragment slot="lead"><img src="/images/groww.svg" class="h-12" alt="groww logo"></svelte:fragment>
+					<svelte:fragment slot="lead"
+						><img src="/images/groww.svg" class="h-12" alt="groww logo" /></svelte:fragment
+					>
 					<svelte:fragment slot="summary">
 						<div class="flex-col">
 							<div>Groww | DevOps Intern</div>
@@ -49,12 +66,21 @@
 					</svelte:fragment>
 					<svelte:fragment slot="content">
 						<ul class="list-disc ml-6">
-							<li>Going to be working on InfraPlatform for the biggest stock broker, with over 50 million users, in India</li>
+							<li>
+								Working on InfraPlatform and Observability for the biggest stock broker, with over
+								50 million users, in India
+							</li>
+							<li>
+								Implemented application change event notifications via Argo CD and canary deployment
+								notifications via Argo Rollouts for internal tools.
+							</li>
 						</ul>
 					</svelte:fragment>
 				</AccordionItem>
 				<AccordionItem>
-					<svelte:fragment slot="lead"><img src="/images/kek.png" class="w-12" alt="kek logo"> </svelte:fragment>
+					<svelte:fragment slot="lead"
+						><img src="/images/kek.png" class="w-12" alt="kek logo" />
+					</svelte:fragment>
 					<svelte:fragment slot="summary">
 						<div class="flex-col">
 							<div>KEK | GRID Computing R&D Intern</div>
@@ -63,14 +89,22 @@
 					</svelte:fragment>
 					<svelte:fragment slot="content">
 						<ul class="list-disc ml-6">
-							<li>Developed a uniform job execution environment using Apptainer, an HPC specialized containerization engine, addressing
-								varied GRID infrastructure and diverse job requirements</li>
-							<li>Documented deployment procedures, ensuring consistent and efficient use of the GRID infrastructure</li>
+							<li>
+								Developed a uniform job execution environment using Apptainer, an HPC specialized
+								containerization engine, addressing varied GRID infrastructure and diverse job
+								requirements
+							</li>
+							<li>
+								Documented deployment procedures, ensuring consistent and efficient use of the GRID
+								infrastructure
+							</li>
 						</ul>
 					</svelte:fragment>
 				</AccordionItem>
 				<AccordionItem>
-					<svelte:fragment slot="lead"><img src="/images/polygon.svg" class="h-12" alt="polygon-labs logo"></svelte:fragment>
+					<svelte:fragment slot="lead"
+						><img src="/images/polygon.svg" class="h-12" alt="polygon-labs logo" /></svelte:fragment
+					>
 					<svelte:fragment slot="summary">
 						<div class="flex-col">
 							<div>Polygon Labs | Protocol Software Engineering Intern</div>
@@ -80,9 +114,15 @@
 					<svelte:fragment slot="content">
 						<ul class="list-disc ml-6">
 							<li>Worked with the Polygon PoS team</li>
-							<li>Developed and optimized client efficiency by writing scripts to test multiple versions of execution and consensus clients
-								simultaneously, reducing the time to identify faulty commits and versions</li>
-							<li>Migrated encoding protocols to gogoproto standards while maintaining compatibility with older amino proto</li>
+							<li>
+								Developed and optimized client efficiency by writing scripts to test multiple
+								versions of execution and consensus clients simultaneously, reducing the time to
+								identify faulty commits and versions
+							</li>
+							<li>
+								Migrated encoding protocols to gogoproto standards while maintaining compatibility
+								with older amino proto
+							</li>
 						</ul>
 					</svelte:fragment>
 				</AccordionItem>
@@ -90,14 +130,18 @@
 		</div>
 	</div>
 
-	<hr class="!border-t-2 !border-yellow-400 my-1">
+	<hr class="!border-t-2 !border-yellow-400 my-1" />
 
 	<div class="flex-col">
-		<h3 class="h3 bg-gradient-to-r from-yellow-400 to-yellow-700 px-4 py-1 text-orange-800">Education</h3>
+		<h3 class="h3 bg-gradient-to-r from-yellow-400 to-yellow-700 px-4 py-1 text-orange-800">
+			Education
+		</h3>
 		<div class="m-2">
 			<Accordion>
 				<AccordionItem>
-					<svelte:fragment slot="lead"><img src="/images/UPES.png" class="h-12" alt="UPES logo"></svelte:fragment>
+					<svelte:fragment slot="lead"
+						><img src="/images/UPES.png" class="h-12" alt="UPES logo" /></svelte:fragment
+					>
 					<svelte:fragment slot="summary">
 						<div class="flex-col">
 							<div>UPES | B.Tech in Computer Science and Engineering</div>
@@ -109,13 +153,17 @@
 							<li>Major in Cloud Computation and Virtualization Technology</li>
 							<li>Minors in DevOps Practices and Design Principles</li>
 							<li>Current GPA: 8.48/10</li>
-							<li>Dean's List for Academic Excellence Award: Recognised for outstanding academic performance during
-								the academic year 2021-2022</li>
+							<li>
+								Dean's List for Academic Excellence Award: Recognised for outstanding academic
+								performance during the academic year 2021-2022
+							</li>
 						</ul>
 					</svelte:fragment>
 				</AccordionItem>
 				<AccordionItem>
-					<svelte:fragment slot="lead"><img src="/images/pba.png" class="h-12" alt="pba logo"></svelte:fragment>
+					<svelte:fragment slot="lead"
+						><img src="/images/pba.png" class="h-12" alt="pba logo" /></svelte:fragment
+					>
 					<svelte:fragment slot="summary">
 						<div class="flex-col">
 							<div>Polkadot Blockchain Academy | Graduate</div>
@@ -125,7 +173,8 @@
 					<svelte:fragment slot="content">
 						<ul class="list-disc ml-6">
 							<li>Graduated from PBA Singapore cohort at NUS.</li>
-							<li>Learnt about:
+							<li>
+								Learnt about:
 								<ul class="list-disc ml-6">
 									<li>Cryptography Basics</li>
 									<li>Economics & Game Theory</li>
@@ -141,10 +190,12 @@
 		</div>
 	</div>
 
-	<hr class="!border-t-2 !border-yellow-400 my-1">
+	<hr class="!border-t-2 !border-yellow-400 my-1" />
 
 	<div class="flex-col">
-		<h3 class="h3 bg-gradient-to-r from-yellow-400 to-yellow-700 px-4 py-1 text-orange-800">Skills and Tools</h3>
+		<h3 class="h3 bg-gradient-to-r from-yellow-400 to-yellow-700 px-4 py-1 text-orange-800">
+			Skills and Tools
+		</h3>
 		<div class="flex-col m-2 items-center justify-center">
 			<h4 class="h4 text-center">Languages</h4>
 			<div class="flex items-center justify-center">
@@ -193,7 +244,7 @@
 		</div>
 	</div>
 
-	<hr class="!border-t-2 !border-yellow-400 my-1">
+	<hr class="!border-t-2 !border-yellow-400 my-1" />
 </div>
 
 <div class="fixed bottom-4 right-4 z-50">
@@ -209,21 +260,21 @@
 </div>
 
 <style>
-    .h1 {
-        background-clip: text;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        background-image: linear-gradient(90deg, #b0e555, #ef9232, #ffdd00);
-        background-size: 400%;
-        animation: gradient-animation 2s infinite alternate linear;
-    }
+	.h1 {
+		background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		background-image: linear-gradient(90deg, #b0e555, #ef9232, #ffdd00);
+		background-size: 400%;
+		animation: gradient-animation 2s infinite alternate linear;
+	}
 
-    @keyframes gradient-animation {
-        0% {
-            background-position: 0%;
-        }
-        100% {
-            background-position: 100%;
-        }
-    }
+	@keyframes gradient-animation {
+		0% {
+			background-position: 0%;
+		}
+		100% {
+			background-position: 100%;
+		}
+	}
 </style>
